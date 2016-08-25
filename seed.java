6456aa9e1285c880ecbaae1a468d25949994f00a
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-25 22:58:07 +0530

    Create seed.java

---
+++ seed.java
@@ -0,0 +1,43 @@
+
+
+import java.util.Scanner;
+
+public class seed
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int n1=0;
+		int pro=0;
+		int f=0;
+		
+		for(int i=1;i<n;i++)
+		{
+			pro=i;
+			if(n%i==0)
+			{
+				n1=i;
+				do
+				{
+					pro=pro*(n1%10);
+					n1=n1/10;
+				}while(n1>0);
+				
+				if(pro==n)
+				{
+					System.out.println(i);
+					f=1;
+				}
+					
+			}
+			
+		}
+		if(f==0)
+			System.out.println("No seed present");
+
+	}
+
+}
